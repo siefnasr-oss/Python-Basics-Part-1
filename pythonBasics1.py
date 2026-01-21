0de5af0a658aa94d89d1d851543fdcbb76e7de30
SieF Nasr <siefnasrb@gmail.com>
2026-01-21 16:04:16 +0200

    Python Basics Part 1

---
+++ pythonBasics1.py
@@ -0,0 +1,58 @@
+if True:
+ print ("hello python")
+
+ #---------------------------------------------------------------------------------
+
+ print ("I Love Python")
+# why comments?
+#--------------------------------
+# Informations About File.
+# Licence.
+# Who Created The File.
+# When The File Created.
+# Why The File Creatd.
+# Write Behind The Programming Line.
+# Write Before/After The Programming Line.
+# Prevent Code From Run.
+#--------------------------------
+
+print ("I Love Python") # I Love C More.
+# print ("I Love Python")
+#-----------------------------------------------------------------------------------
+
+print(type(10)) #int
+print (type(10.9)) #float
+print(type("hello python")) #str
+print(type([10,20,30,40])) #list
+print(type((10,20,30,40))) #tuple
+print(type({"one" : 1,"two" : 2,"Three" : 3})) #dict
+print(type(2 == 2)) #bool
+#-----------------------------------------------------------------------------------
+
+x = 9
+print(x)
+_y = "Sief Nasr"
+print(_y)
+name = "Sief"
+print(name)
+
+name = "Sief Nasr" # Single Word => Normal
+MyName = "Sief Nasr" # Two Words => CamelCase
+My_Name = "Sief Nasr" #Two Words => Snake_Case
+print(name)
+print(MyName)
+print(My_Name)
+#---------------------------------------------------
+x = 10
+x = "hello"
+print(x)
+
+# Reserved Words
+help("keywords")
+
+# Mult Variables
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+#------------------------------------------------------------------------------------
